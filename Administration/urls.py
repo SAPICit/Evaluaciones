efe3c962834fdfb493fd8bb36f2b02593c10757a
Osvaldo <ocordoba@cesehsa.com>
2026-05-21 16:53:44 -0600

    Add form creation evaluation

--- Administration/urls.py
+++ Administration/urls.py
@@ -6,4 +6,5 @@ app_name = 'Administration'
 urlpatterns = [
     path('MyEvaluations/', MyEvaluationManager.as_view(), name='MyEvaluations'),
     path('EvaluationManagement/', EvaluationManagement.as_view(), name='EvaluationManagement'),
+    path('ControlEvaluation/<str:action>/', ControlEvaluation.as_view(), name='ControlEvaluation'),
 ]
--- Administration/views.py
+++ Administration/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from .forms import AreasForm,AreasEfectividadFormSet,CalidadOperativaFormSet,CulturaLaboralFormSet, TiposEvaluacionesFormSet
+from django.urls import reverse_lazy
+from evaluaciones.models import Areas,TiposEvaluaciones
 
 
 class MyEvaluationManager(LoginRequiredMixin, TemplateView):
@@ -9,3 +12,33 @@ class MyEvaluationManager(LoginRequiredMixin, TemplateView):
 
 class EvaluationManagement(LoginRequiredMixin, TemplateView):
     template_name = 'evaluation_management.html'
+
+class ControlEvaluation(LoginRequiredMixin, TemplateView):
+    template_name = 'creation_update_evaluation.html'
+
+    def get(self, request, *args, **kwargs):
+
+        context = {
+
+            'TiposEvaluaciones_formset': TiposEvaluacionesFormSet(
+                queryset=TiposEvaluaciones.objects.none(),
+                prefix='TiposEvaluaciones'
+            ),
+
+            'AreasEfectividad_formset': AreasEfectividadFormSet(
+                queryset=Areas.objects.none(),
+                prefix='AreasEfectividad'
+            ),
+
+            'CalidadOperativa_formset': CalidadOperativaFormSet(
+                queryset=Areas.objects.none(),
+                prefix='CalidadOperativa'
+            ),
+
+            'CulturaLaboral_formset': CulturaLaboralFormSet(
+                queryset=Areas.objects.none(),
+                prefix='CulturaLaboral'
+            ),
+        }
+
+        return self.render_to_response(context)
